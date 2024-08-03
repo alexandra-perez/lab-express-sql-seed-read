87a643eedeaac44b3ba487e0261bba3b3e3d9b44
alexandra-perez <alexandraperezny@gmail.com>
2024-08-03 17:22:40 -0400

    Searchbar and new form CSS

--- frontend/src/Components/CreateForm/CreateForm.jsx
+++ frontend/src/Components/CreateForm/CreateForm.jsx
@@ -3,10 +3,24 @@ import './CreateForm.scss';
 export default function CreateForm() {
   return (
     <form className="CreateForm">
-      <label htmlFor="songName"> Song Name</label>
+      <h1>Add a new Song</h1>
+      <label htmlFor="songName">Title</label>
       <input type="text" id="songName" name="songName" />
-      <label htmlFor="songArtist"> Song Artist</label>
+      <br />
+      <label htmlFor="songArtist">Artist</label>
       <input type="text" id="songArtist" name="songArtist" />
+      <br />
+      <label htmlFor="songAlbum">Album</label>
+      <input type="text" id="songAlbum" name="songAlbum" />
+      <br />
+      <label htmlFor="songDuration">Duration</label>
+      <input type="number" name="songDuration" id="songDuration" />
+      <br />
+      <div className="container">
+        <label htmlFor="songIsFavorite">Add to favorites</label>
+        <input type="checkbox" name="songIsFavorite" id="songIsFavorite" />
+      </div>
+      <button>Add song</button>
     </form>
   );
 }
