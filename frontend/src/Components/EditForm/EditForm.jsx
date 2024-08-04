8f34d6fcd68c47cb116e50a5e76e3d355bdc38f0
alexandra-perez <alexandraperezny@gmail.com>
2024-08-04 16:26:08 -0400

    Add usestate to edit form

--- frontend/src/Components/EditForm/EditForm.jsx
+++ frontend/src/Components/EditForm/EditForm.jsx
@@ -1,25 +1,54 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import './EditForm.scss';
+
 export default function EditForm() {
+  const [currentSong, setCurrentSong] = useState({
+    name: '',
+    artist: '',
+    album: '',
+    time: '',
+    is_favorite: '',
+  });
+  const API = import.meta.env.VITE_API_URL;
+  let { id } = useParams();
+
+  useEffect(() => {
+    fetch(`${API}/songs/${id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resJSON) => setCurrentSong(resJSON))
+      .then((resJSON) => console.log('currentSong =', currentSong))
+      .catch((error) => console.error(error));
+  }, []);
+
+  function handleTextChange(e) {}
+
+  function handleSubmit(e) {}
+
   return (
     <div className="EditForm">
       <h1>Edit song</h1>
       <form>
-        <label htmlFor="songName">Title</label>
-        <input type="text" id="songName" name="songName" />
+        <label htmlFor="name">Title</label>
+        <input type="text" id="name" name="name" />
         <br />
-        <label htmlFor="songArtist">Artist</label>
-        <input type="text" id="songArtist" name="songArtist" />
+        <label htmlFor="artist">Artist</label>
+        <input type="text" id="artist" name="artist" />
         <br />
-        <label htmlFor="songAlbum">Album</label>
-        <input type="text" id="songAlbum" name="songAlbum" />
+        <label htmlFor="album">Album</label>
+        <input type="text" id="album" name="album" />
         <br />
-        <label htmlFor="songDuration">Duration</label>
-        <input type="number" name="songDuration" id="songDuration" />
+        <label htmlFor="time">Duration</label>
+        <input type="number" name="time" id="time" />
         <br />
         <div className="container">
-          <label htmlFor="songIsFavorite">Favorite</label>
-          <input type="checkbox" name="songIsFavorite" id="songIsFavorite" />
+          <label htmlFor="is_favorite">Favorite</label>
+          <input type="checkbox" name="is_favorite" id="is_favorite" />
         </div>
-        <button>Add song</button>
+        <button>Submit</button>
       </form>
     </div>
   );
